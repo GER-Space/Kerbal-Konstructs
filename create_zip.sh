081808aa5839b28330e1946e3c85879d076a6324
Christian Bronk <chris.github@bronk.de>
2016-11-28 22:42:42 +0100

    add post script

---
+++ create_zip.sh
@@ -0,0 +1,16 @@
+#/usr/bin/bash
+#
+#
+
+VERSION=`grep -i AssemblyFileVersion src/Properties/AssemblyInfo.cs  | cut -d "\"" -f 2`
+
+
+FILENAME="Kerbal-Konstructs-${VERSION}.zip"
+
+if test -e $FILENAME ; then 
+	rm -f  Kerbal-Konstructs-${VERSION}.zip
+fi
+
+7z a -r  $FILENAME GameData
+
+
