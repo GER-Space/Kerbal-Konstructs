3f55fd9949265d9d02e7870620caadf8e84e99f7
Christian Bronk <chris.github@bronk.de>
2017-04-26 23:43:15 +0200

    Flag Decal Support

---
+++ src/Core/StaticObjects/StaticModules/FlagDecal/FlagDecal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KerbalKonstructs.Core;
+using UnityEngine;
+
+
+namespace KerbalKonstructs
+{
+    public class FlagDecal: StaticModule
+    {
+
+        public string textureQuadName;
+
+        public void Awake()
+        {
+            Log.Normal("FlagDeclal: Awake called");
+            //setTexture();
+        }
+
+        public override void StaticObjectUpdate()
+        {
+            Log.Normal("FlagDeclal: Set Texture Called");
+            setTexture();
+        }
+        internal void setTexture ()
+        {
+            Transform transform = gameObject.transform.FindChild(textureQuadName);
+            Renderer flagRenderer = transform.GetComponent<Renderer>();
+            flagRenderer.material.mainTexture = GameDatabase.Instance.GetTexture(HighLogic.CurrentGame.flagURL, false);
+        }
+
+
+
+    }
+}
