7038b3de24a6399ec7681ef6aab6b266a486e910
Christian Bronk <chris.ksp@bronk.de>
2018-03-07 11:44:03 +0100

    remove unneeded functions and updates License

--- src/Modules/Career/CareerUtils.cs
+++ src/Modules/Career/CareerUtils.cs
@@ -7,21 +7,6 @@ namespace KerbalKonstructs.Modules
     internal static class CareerUtils
     {
 
-        private static List<string> stringAttributes = new List<string>{
-            "FacilityType", "InStorage", "TargetID", "TargetType" , "Producing", "OpenCloseState", "missionlog" , "openclosestate" , "favouritesite" };
-
-        private static List<string> floatAttributes = new List<string>{
-            "FacilityXP",
-            "StaffMax", "StaffCurrent", "LqFCurrent", "OxFCurrent", "MoFCurrent",
-            "ECCurrent", "OreCurrent", "PrOreCurrent", "ScienceOMax", "ScienceOCurrent",
-            "RepOMax", "RepOCurrent", "FundsOMax", "FundsOCurrent", "LastCheck",
-            "ProductionRateMax", "ProductionRateCurrent", "MissionCount",
-            "missioncount" };
-
-        private static List<string> vector3Attributes = new List<string> { "RadialPosition" };
-
-        private static List<string> boolAttributes = new List<string>();
-
 
         internal static bool isCareerGame
         {
@@ -37,93 +22,6 @@ namespace KerbalKonstructs.Modules
             {
                 return ((HighLogic.CurrentGame.Mode != Game.Modes.CAREER) || (KerbalKonstructs.instance.disableCareerStrategyLayer));
             }
-        }
-
-        /// <summary>
-        /// Returns a list with the neccessary parameters for a facility
-        /// </summary>
-        /// <param name="facType"></param>
-        /// <returns></returns>
-        internal static List<string> ParametersForFacility(string facType)
-        {
-            List<string> paramList = new List<string> { "OpenCloseState" };
-
-
-            switch (facType)
-            {
-                case "Barracks":
-                    paramList.Add("StaffCurrent");
-                    break;
-                case "FuelTanks":
-                    paramList.Add("LqFCurrent");
-                    paramList.Add("OxFCurrent");
-                    paramList.Add("MoFCurrent");
-                    break;
-                case "Hangar":
-                    paramList.Add("InStorage");
-                    paramList.Add("TargetID");
-                    paramList.Add("TargetType");
-                    break;
-                case "Business":
-                    paramList.Add("StaffCurrent");
-                    paramList.Add("FundsOCurrent");
-                    break;
-                case "Research":
-                    paramList.Add("StaffCurrent");
-                    paramList.Add("ScienceOCurrent");
-                    break;
-                default:
-                    break;
-            }
-
-            return paramList;
-        }
-
-        /// <summary>
-        /// returns a list with the necessarry parameters for a LaunchSite
-        /// </summary>
-        /// <returns></returns>
-        internal static List<string> ParametersForLaunchSite()
-        {
-            List<string> paramList = new List<string> { "openclosestate", "favouritesite", "missioncount", "missionlog" };
-            return paramList;
-        }
-
-        /// <summary>
-        /// Checks if a facility should be treated as "Open"
-        /// </summary>
-        /// <param name="instance"></param>
-        /// <returns></returns>
-        internal static bool FacilityIsOpen(StaticInstance instance)
-        {
-            if (instance.myFacilities[0].isOpen)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        internal static bool IsString (string parameter)
-        {
-            return stringAttributes.Contains(parameter);
-        }
-
-        internal static bool IsFloat(string parameter)
-        {
-            return floatAttributes.Contains(parameter);
-        }
-
-        internal static bool IsVector3(string parameter)
-        {
-            return vector3Attributes.Contains(parameter);
-        }
-   
-        internal static bool IsBool(string parameter)
-        {
-            return boolAttributes.Contains(parameter);
         }
 
         internal static string KeyFromString(string orginalString)
