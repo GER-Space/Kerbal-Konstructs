023ee1d0f3e19bdb310477011a5b87148bea151f
Christian Bronk <chris.ksp@bronk.de>
2018-03-14 14:31:29 +0100

    try to update the statics

--- src/KerbalKonstructs.cs
+++ src/KerbalKonstructs.cs
@@ -497,6 +497,7 @@ namespace KerbalKonstructs
                 {
                     CareerState.Load(careerNode.GetNode("KerbalKonstructsSettings"));
                 }
+                updateCache();
             }
             
 
