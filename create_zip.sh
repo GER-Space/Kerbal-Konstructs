cc8db095caf002b9fe0148649e63189848e4e624
GER-Space <chris.github@bronk.de>
2018-08-18 08:52:04 +0200

    Automate versioning in .version file: fixes: #110

--- create_zip.sh
+++ create_zip.sh
@@ -9,9 +9,22 @@ VERSION=`grep -i AssemblyFileVersion src/Properties/AssemblyInfo.cs  | cut -d "\
 
 FILENAME="Kerbal-Konstructs-${VERSION}.zip"
 
+MAJOR=`echo ${VERSION} | cut -d "." -f 1 `
+MINOR=`echo ${VERSION} | cut -d "." -f 2 `
+PATCH=`echo ${VERSION} | cut -d "." -f 3 `
+BUILD=`echo ${VERSION} | cut -d "." -f 4 `
+
+
 if test -e $FILENAME ; then 
 	rm -f  Kerbal-Konstructs-${VERSION}.zip
 fi
+
+
+sed -i "s/MAJOR\":./MAJOR\":$MAJOR/" GameData/KerbalKonstructs/KerbalKonstructs.version
+sed -i "s/MINOR\":./MINOR\":$MINOR/" GameData/KerbalKonstructs/KerbalKonstructs.version
+sed -i "s/PATCH\":./PATCH\":$PATCH/" GameData/KerbalKonstructs/KerbalKonstructs.version
+sed -i "s/BUILD\":./BUILD\":$BUILD/" GameData/KerbalKonstructs/KerbalKonstructs.version
+
 
 /c/Program\ Files/7-Zip/7z.exe a -r  $FILENAME GameData
 
