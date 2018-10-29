5713ad320c0936774129f900809eda34006a47a8
GER-Space <chris.github@bronk.de>
2018-10-29 11:25:01 +0100

    fix for Advanced Texture returning no image

--- src/Utilities/KKGraphics.cs
+++ src/Utilities/KKGraphics.cs
@@ -19,6 +19,9 @@ namespace KerbalKonstructs
 
         private static List<string> imageExtentions = new List<string> { ".png", ".tga", ".jpg" };
 
+
+
+
         /// <summary>
         /// Load all shaders into the system and fill our shader database.
         /// </summary>
@@ -35,7 +38,7 @@ namespace KerbalKonstructs
             loadedShaders = true;
         }
 
-        internal static bool HasShader (string name)
+        internal static bool HasShader(string name)
         {
             if (!loadedShaders)
             {
@@ -63,7 +66,7 @@ namespace KerbalKonstructs
             }
             else
             {
-                Log.UserError("Shader not found: " + name);
+                Log.UserError("AdvTexture: Shader not found: " + name);
                 // return the error Shader, if we have one
                 if (allShaders.ContainsKey("Hidden/InternalErrorShader"))
                 {
@@ -102,16 +105,26 @@ namespace KerbalKonstructs
                 return cachedTextures[textureKey];
             }
 
+            List<Texture> foundTextures = null;
             Texture2D foundTexture = null;
             ;
             if (textureName.StartsWith("BUILTIN:"))
             {
                 string textureNameShort = Regex.Replace(textureName, "BUILTIN:/", "");
-                foundTexture = Resources.FindObjectsOfTypeAll<Texture>().Where(tex => tex.name == textureNameShort).ToList()[index] as Texture2D;
-                if (foundTexture == null)
+                foundTextures = Resources.FindObjectsOfTypeAll<Texture>().Where(tex => tex.name == textureNameShort).ToList() as List<Texture>;
+
+                if (foundTextures.Count == 0)
                 {
-                    Log.UserError("Could not find built-in texture " + textureNameShort);
+                    Log.UserError("AdvTexture: Could not find built-in texture " + textureNameShort);
+                    return null;
                 }
+                if (foundTextures.Count < index + 1)
+                {
+                    Log.UserError("AdvTexture: index out of range" + textureNameShort + " : " + index);
+                    return null;
+                }
+                return foundTextures[index] as Texture2D;
+
             }
             else
             {
@@ -130,7 +143,7 @@ namespace KerbalKonstructs
                 }
                 else
                 {
-                    Log.UserWarning("TextureLoader faild. Fallback to GameDatabase");
+                    Log.UserWarning("AdvTexture: TextureLoader faild. Fallback to GameDatabase");
                     foundTexture = GameDatabase.Instance.GetTexture(textureName, asNormal);
                 }
 
@@ -144,6 +157,36 @@ namespace KerbalKonstructs
             cachedTextures.Add(textureKey, foundTexture);
             return foundTexture;
         }
+
+        internal static Material GetMaterial(string materialName)
+        {
+            if (!loadedMaterials)
+            {
+                foreach (Material material in Resources.FindObjectsOfTypeAll<Material>())
+                {
+                    if (!cachedMaterials.ContainsKey(material.name))
+                    {
+                        cachedMaterials.Add(material.name, material);
+                    }
+                }
+
+                loadedMaterials = true;
+            }
+
+            if (cachedMaterials.ContainsKey(materialName))
+            {
+                return cachedMaterials[materialName];
+            }
+            if (cachedMaterials.ContainsKey(materialName + " (Instance)"))
+            {
+                return cachedMaterials[materialName + " (Instance)"];
+            }
+
+            Log.UserError("AdvTexture: No Material found: " + materialName);
+            return null;
+        }
+
+
 
         private static string GetImageExtention(string imageName)
         {
@@ -167,35 +210,10 @@ namespace KerbalKonstructs
                 }
             }
 
-            Log.UserError("Could not find an image with the name: " + imageName);
+            Log.UserError("AdvTexture: Could not find an image with the name: " + imageName);
             return null;
         }
 
-
-        internal static Material GetMaterial(string materialName)
-        {
-            if (!loadedMaterials)
-            {
-                foreach (Material material in Resources.FindObjectsOfTypeAll<Material>())
-                {
-                    cachedMaterials.Add(material.name, material);
-                }
-
-                loadedMaterials = true;
-            }
-
-            if (cachedMaterials.ContainsKey(materialName))
-            {
-                return cachedMaterials[materialName];
-            }
-            if (cachedMaterials.ContainsKey(materialName + " (Instance)"))
-            {
-                return cachedMaterials[materialName + " (Instance)"];
-            }
-
-            Log.UserError("No Material found: " + materialName);
-            return null;
-        }
 
     }
 }
